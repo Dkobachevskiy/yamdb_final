8822c136d10fa248c91f2e8a6a4689e37318684f
Denis Kobachevskiy <dkobachevskiy@gmail.com>
2021-06-16 23:44:22 +0300

    final 19_37

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['84.252.143.105']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
